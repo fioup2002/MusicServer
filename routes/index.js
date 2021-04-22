952611b9a1e22c12fd18321d942185ae37d6c1c5
Ice.Liu <ice.liu@ihwic.com>
2021-04-22 11:49:01 +0800

    1. add phantom package

--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,49 @@
-var express = require("express");
-var google = require("../google/main.js");
-var router = express.Router();
+import { Router } from "express";
+import { GetFileList, SetCode, IsNeedAccess, GetAccessURL, GetDownloadStatus } from "../google/main.js";
+var router = Router();
+import { create } from "phantom";
+import jsdom from "jsdom";
+const { JSDOM } = jsdom;
+
+var gHtml = "";
 
 router.get("/", function (req, res, next) {
   res.redirect("/index.html");
 });
 router.get("/GetFileList", function (req, res, next) {
-  google.GetFileList(function (type, str) {
+  GetFileList(function (type, str) {
     SendResponse(type, str, res);
   });
 });
 router.get("/SetCode", function (req, res, next) {
-  google.SetCode(req.query.code, function (type, str) {
+  SetCode(req.query.code, function (type, str) {
     SendResponse(type, str, res);
   });
 });
 router.get("/IsNeedAccess", function (req, res, next) {
-  google.IsNeedAccess(function (type, str) {
+  IsNeedAccess(function (type, str) {
     SendResponse(type, str, res);
   });
 });
 router.get("/GetAccessURL", function (req, res, next) {
-  google.GetAccessURL(function (type, str) {
+  GetAccessURL(function (type, str) {
     SendResponse(type, str, res);
   });
 });
 router.get("/GetDownloadStatus", function (req, res, next) {
-  google.GetDownloadStatus(function (type, str) {
+  GetDownloadStatus(function (type, str) {
     SendResponse(type, str, res);
   });
 });
 router.get("/favicon.ico", function (req, res, next) {
   res.send("");
 });
- 
 
-module.exports = router;
+router.get("/getHtml", function (req, res, next) {
+  res.send("");
+});
+
+export default router;
 
 function SendResponse(type, str, res) {
   var obj = new Object();
@@ -43,3 +51,23 @@ function SendResponse(type, str, res) {
   obj.content = str;
   res.send(JSON.stringify(obj));
 }
+function GetHtml() {
+  create().then(function (ph) {
+    ph.createPage().then(function (page) {
+      page.open("https://www.gffx188.com/news?type=tips").then(function (status) {
+        page.property("content").then(function (content) {
+          const { window } = new JSDOM(content);
+          var $ = require('jquery')(window);
+          for (var i = 0; i < $(".content-module--item--1Jgbx").length; i++){
+            console.log($($(".content-module--item--1Jgbx")[i]).children(".content-module--itemMain--wd5XD").html());
+          }
+          page.close().then(function () {
+            ph.exit();
+          });
+        });
+      });
+    });
+  });
+}
+
+GetHtml();
