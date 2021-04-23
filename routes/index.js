73bc0a29e94b898d2c97559de13322e6e2c9140a
Ice.Liu <ice.liu@ihwic.com>
2021-04-23 13:47:15 +0800

    1. change import way

--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,46 @@
-import { Router } from "express";
-import { GetFileList, SetCode, IsNeedAccess, GetAccessURL, GetDownloadStatus } from "../google/main.js";
-var router = Router();
-import { create } from "phantom";
-import jsdom from "jsdom";
-const { JSDOM } = jsdom;
+var express = require("express");
+var google = require("../google/main.js");
+var router = express.Router();
+var jsdom  = require("jsdom");
+var phantom = require("phantom");
 
 var gHtml = "";
-
 router.get("/", function (req, res, next) {
   res.redirect("/index.html");
 });
 router.get("/GetFileList", function (req, res, next) {
-  GetFileList(function (type, str) {
+  google.GetFileList(function (type, str) {
     SendResponse(type, str, res);
   });
 });
 router.get("/SetCode", function (req, res, next) {
-  SetCode(req.query.code, function (type, str) {
+  google.SetCode(req.query.code, function (type, str) {
     SendResponse(type, str, res);
   });
 });
 router.get("/IsNeedAccess", function (req, res, next) {
-  IsNeedAccess(function (type, str) {
+  google.IsNeedAccess(function (type, str) {
     SendResponse(type, str, res);
   });
 });
 router.get("/GetAccessURL", function (req, res, next) {
-  GetAccessURL(function (type, str) {
+  google.GetAccessURL(function (type, str) {
     SendResponse(type, str, res);
   });
 });
 router.get("/GetDownloadStatus", function (req, res, next) {
-  GetDownloadStatus(function (type, str) {
+  google.GetDownloadStatus(function (type, str) {
     SendResponse(type, str, res);
   });
 });
 router.get("/favicon.ico", function (req, res, next) {
   res.send("");
 });
-
 router.get("/getHtml", function (req, res, next) {
   res.send("");
 });
 
-export default router;
+module.exports = router;
 
 function SendResponse(type, str, res) {
   var obj = new Object();
@@ -51,12 +48,13 @@ function SendResponse(type, str, res) {
   obj.content = str;
   res.send(JSON.stringify(obj));
 }
+
 function GetHtml() {
-  create().then(function (ph) {
+  phantom.create().then(function (ph) {
     ph.createPage().then(function (page) {
       page.open("https://www.gffx188.com/news?type=tips").then(function (status) {
         page.property("content").then(function (content) {
-          const { window } = new JSDOM(content);
+          const { window } = new jsdom.JSDOM(content);
           var $ = require('jquery')(window);
           for (var i = 0; i < $(".content-module--item--1Jgbx").length; i++){
             console.log($($(".content-module--item--1Jgbx")[i]).children(".content-module--itemMain--wd5XD").html());
