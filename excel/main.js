1ca5d19f508744025e3928f076a94c81c38331b6
Ice.Liu <ice.liu@ihwic.com>
2021-12-06 09:08:01 +0800

    1. 更新寫檔的格式

--- excel/main.js
+++ excel/main.js
@@ -2674,34 +2674,34 @@ var data = {
 };
 
 const Excel = require("exceljs");
-
-// Create workbook & add worksheet
 const workbook = new Excel.Workbook();
-const worksheet = workbook.addWorksheet("ExampleSheet")
-console.log(data.productTable[0])
+const worksheet = workbook.addWorksheet("工作表");
 // add column headers
-worksheet.columns = [
-  { header: "Package", key: "package_name" },
-  { header: "Author", key: "author_name" },
-];
+worksheet.columns = [{ header: "中文名稱" }, { header: "英文名稱" }, { header: "類型" }, { header: "是否在選單" }, { header: "最低手數" }, { header: "貨幣單位" }, { header: "合約規格" }, { header: "單筆交易手數" }, { header: "日内初始保证金" }, {}, {}, {}, { header: "日内强平保证金" }, {}, {}, {}, { header: "周末/假期强平保证金" }, {}, {}, {}, { header: "交易時間" }];
+worksheet.columns.forEach((obj, i) => {
+  obj.width = 15;
+  if (i == 11 || i == 15 || i == 19 || i == worksheet.columns.length - 1) {
+    obj.width = 40;
+  }
+  obj.alignment = { vertical: "middle", horizontal: "center", wrapText: true };
+});
+worksheet.mergeCells(1, 9, 1, 12);
+worksheet.mergeCells(1, 13, 1, 16);
+worksheet.mergeCells(1, 17, 1, 20);
+data.productTable.forEach((obj, i) => {
+  var content = obj.detail.content;
+  var deposit = content.deposit;
+  worksheet.addRow([obj.name.ch, obj.name.en, obj.type, obj.isInCheckbox ? "V" : "", content.minTrade, content.currency, content.spec, content.singleTrade, "最低", "最高", "獲利", "文字", "最低", "最高", "獲利", "文字", "最低", "最高", "獲利", "文字", content.tradeTime]);
+  if (deposit != undefined) {
+    worksheet.addRow([, , , , , , , , , deposit[0][0].min, deposit[0][0].max, deposit[0][0].profit, deposit[0][0].text, deposit[1][0].min, deposit[1][0].max, deposit[1][0].profit, deposit[1][0].text, deposit[2][0].min, deposit[2][0].max, deposit[2][0].profit, deposit[2][0].text]);
+    worksheet.addRow([, , , , , , , , , deposit[0][1].min, deposit[0][1].max, deposit[0][1].profit, deposit[0][1].text, deposit[1][1].min, deposit[1][1].max, deposit[1][1].profit, deposit[1][1].text, deposit[2][1].min, deposit[2][1].max, deposit[2][1].profit, deposit[2][1].text]);
+    worksheet.addRow([, , , , , , , , , deposit[0][2].min, deposit[0][2].max, deposit[0][2].profit, deposit[0][2].text, deposit[1][2].min, deposit[1][2].max, deposit[1][2].profit, deposit[1][2].text, deposit[2][2].min, deposit[2][2].max, deposit[2][2].profit, deposit[2][2].text]);
+  }
+  for (var j = 1; j <= 8; j++) {
+    worksheet.mergeCells(4 * i + 2, j, 4 * i + 5, j);
+  }
+  worksheet.mergeCells(4 * i + 2, 21, 4 * i + 5, 21);
+});
 
-// Add row using key mapping to columns
-worksheet.addRow({ package_name: "ABC", author_name: "Author 1" }, { package_name: "XYZ", author_name: "Author 2" });
+workbook.xlsx.writeFile("前端用表格.xlsx");
 
-// Add rows as Array values
-worksheet.addRow(["BCD", "Author Name 3"]);
-
-// Add rows using both the above of rows
-const rows = [["FGH", "Author Name 4"], { package_name: "PQR", author_name: "Author 5" }];
-
-worksheet.addRows(rows);
-
-// save workbook to disk
-workbook.xlsx
-  .writeFile("sample.xlsx")
-  .then(() => {
-    console.log("saved");
-  })
-  .catch((err) => {
-    console.log("err", err);
-  });
