0fc195adeb8c1e4c9f877ab77ad2ed1631255133
Ice.Liu <ice.liu@ihwic.com>
2022-07-22 13:37:18 +0800

    1. 調整樣式

--- public/js/token.js
+++ public/js/token.js
@@ -10,7 +10,7 @@ new Vue({
     pass: "abc123",
     passList: ["abc123", "eKu3dDCp758rt7QaH3QmIfbv9shIYh4Q"],
     url: "https://openapi.hxfx.com/tools/?",
-    urlList: ["https://openapi.hxfx.com/tools/?", "https://openapi.gffx168.com/tools/?", "https://openapi.washingtou2022.com/tools/?","http://192.168.75.48:4101/tools/?","http://192.168.75.52:6201/cms/?","http://openapi.s89cloud.com/tools/?"],
+    urlList: ["https://openapi.hxfx.com/tools/?", "https://openapi.gffx168.com/tools/?", "https://openapi.washingtou2022.com/tools/?","http://openapi.s89cloud.com/tools/?"],
     sign: "",
     token: "",
   },
